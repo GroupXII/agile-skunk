07d3f7ec309d20591275fd83e0a7881afa4a0fb1
seleenah <seleenah@ASB9820u-B03.csil.sfu.ca>
2015-10-16 19:25:15 -0700

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+
+
+  root'static_pages#home'
+
+
+
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
