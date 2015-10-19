ba31ef0ebd0762e6935dd498b62e9ae473187753
seleenah <seleenah@asb9838su-d02.csil.sfu.ca>
2015-10-19 12:07:42 -0700

    changed links, added logout button and welcome thing

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
+  get  'userhome' => 'users#index' 
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,10 @@ ActiveRecord::Schema.define(version: 20151018081209) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
+    t.string   "password_confirmation"
     t.string   "remember_digest"
   end
 
