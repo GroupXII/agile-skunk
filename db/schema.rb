fb493b669c9125b42a198d3d8be6f6ce554acd81
Evan Chisholm <evan.chisholm@gmail.com>
2015-10-21 14:47:34 -0700

    heroku deployment + gemfile edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015205905) do
+ActiveRecord::Schema.define(version: 20151018081209) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
